5bd4c76fbea8d6dc917377704048d924821f361d
Yuto Takei <y.takei@is.s.u-tokyo.ac.jp>
2012-07-14 16:55:47 +0900

    Slight optimization

--- Program.cs
+++ Program.cs
@@ -2,25 +2,28 @@
 
 class Program
 {
+    static Random rnd = new Random(100);
+
     static void Main(string[] args)
     {
-        for (var i = 0; i < 10000; i++)
-        {
-            var isPrime = IsPrime(i);
-            if (SolovayStrassen(i) != isPrime)
-                Console.WriteLine(isPrime ? "ERROR {0}" : "Mistake {0}", i);
-        }
+        var primes = Eratosthenes(40000);
+        for (var i = 3; i < 40000; i += 2)
+            if (SolovayStrassen(i) != primes[i])
+                Console.WriteLine(primes[i] ? "ERROR {0}" : "Mistake {0}", i);
     }
 
-    static bool IsPrime(int n)
+    static bool[] Eratosthenes(int n)
     {
-        if (n == 2) return true;
-        else if (n < 2 || (n & 1) == 0) return false;
+        var ret = new bool[n];
+        ret[2] = true;
+        for (var i = 3; i < n; i += 2) ret[i] = true;
 
         var u = (int)Math.Floor(Math.Sqrt(n)) + 1;
-        for (var t = 3; t < u; t++)
-            if (n % t == 0) return false;
-        return true;
+        for (var i = 3; i < u; i += 2)
+            for (var j = i * i; j < n; j += i)
+                ret[j] = false;
+
+        return ret;
     }
 
     static bool SolovayStrassen(int n)
@@ -28,11 +31,10 @@ class Program
         if (n == 2) return true;
         else if (n < 2 || (n & 1) == 0) return false;
 
-        var rnd = new Random();
         var a = rnd.Next(1, n);
 
         if (Gcd(a, n) != 1) return false;
-        else return PowMod(a, (n - 1) / 2, n) == ((Jacobi(a, n) % n) + n) % n;
+        else return PowMod(a, (n - 1) / 2, n) == Jacobi(a, n);
     }
 
     static int Gcd(int a, int b)
@@ -55,25 +57,26 @@ class Program
 
     static int Jacobi(int a, int n)
     {
-        if (a < 0)
-            switch (n % 4)
-            {
-                case 1: return Jacobi(-a, n);
-                case 3: return -Jacobi(-a, n);
-            }
-        if (a < 2) return a;
+        var flag = false;
+        var N = n;
+
+    Start:
+        if (a < 2) return flag ? N - a : a;
         if ((a & 1) == 0)
         {
             switch (n % 8)
             {
                 case 1:
-                case 7: return Jacobi(a / 2, n);
+                case 7: a /= 2; goto Start;
                 case 3:
-                case 5: return -Jacobi(a / 2, n);
+                case 5: flag = !flag; a /= 2; goto Start;
             }
         }
-        else if (a % 4 == 3 && n % 4 == 3) return -Jacobi(n % a, a);
-        return Jacobi(n % a, a);
+        else if (a % 4 == 3 && n % 4 == 3) flag = !flag;
+        var t = a;
+        a = n % a;
+        n = t;
+        goto Start;
     }
 
     static int PowMod(int @base, int pow, int mod)
