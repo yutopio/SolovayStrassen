042eca7b4d6b0b97ee743703a732e0dd67237794
Yuto Takei <y.takei@is.s.u-tokyo.ac.jp>
2012-07-14 16:08:42 +0900

    Implemented

--- Program.cs
+++ Program.cs
@@ -4,5 +4,82 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Write("Input: ");
+        var input = Console.ReadLine();
+        Console.WriteLine();
+
+        int n;
+        if (!int.TryParse(input, out n)) return;
+        Console.WriteLine(SolovayStrassen(n) ? "Probably PRIME" : "COMPOSITE");
+    }
+
+    static bool SolovayStrassen(int n)
+    {
+        if (n == 2) return true;
+        else if (n < 2 || (n & 1) == 0) return false;
+
+        var rnd = new Random();
+        var a = rnd.Next(1, n);
+
+        if (Gcd(a, n) != 1) return false;
+        else return PowMod(a, (n - 1) / 2, n) == ((Jacobi(a, n) % n) + n) % n;
+    }
+
+    static int Gcd(int a, int b)
+    {
+        if (a <= 0 || b <= 0)
+            throw new ArgumentOutOfRangeException();
+        else if (a > b) goto StartB;
+
+    StartA:
+        // b >= a > 0
+        b %= a;
+        if (b == 0) return a;
+
+    StartB:
+        // a > b > 0
+        a %= b;
+        if (a == 0) return b;
+        goto StartA;
+    }
+
+    static int Jacobi(int a, int n)
+    {
+        if (a < 0)
+            switch (n % 4)
+            {
+                case 1: return Jacobi(-a, n);
+                case 3: return -Jacobi(-a, n);
+            }
+        if (a < 2) return a;
+        if ((a & 1) == 0)
+        {
+            switch (n % 8)
+            {
+                case 1:
+                case 7: return Jacobi(a / 2, n);
+                case 3:
+                case 5: return -Jacobi(a / 2, n);
+            }
+        }
+        else if (a % 4 == 3 && n % 4 == 3) return -Jacobi(n % a, a);
+        return Jacobi(n % a, a);
+    }
+
+    static int PowMod(int @base, int pow, int mod)
+    {
+        if (@base < 0 || pow < 0 || mod <= 0 || mod > 46340)
+            throw new ArgumentOutOfRangeException();
+        @base %= mod;
+
+        var ret = 1;
+        while (pow != 0)
+        {
+            if ((pow & 1) == 1)
+                ret = (ret * @base) % mod;
+            @base = (@base * @base) % mod;
+            pow >>= 1;
+        }
+        return ret;
     }
 }
